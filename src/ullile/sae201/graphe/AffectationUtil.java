7feb0fc82e2ff8761d534818c50f45b226fe093a
Elise Leroy <elise.leroy2.etu@univ-lille.fr>
2023-05-15 11:35:00 +0200

    [Modified] typo, incoherences

--- src/ullile/sae201/graphe/AffectationUtil.java
+++ src/ullile/sae201/graphe/AffectationUtil.java
@@ -5,11 +5,13 @@ import ullile.sae201.exception.RequirementNotFound;
 
 /**
  * AffectationUtil class
- * @author Romain Degez, Valentin Thuillier
+ * @author Romain Degez, Valentin Thuillier, Eise Leroy
  */
 public class AffectationUtil {
 
-    private static final int MAXHOBBIESCOUNT = 3;
+    private static final int MAX_HOBBIES_COUNT = 3;
+    private static final int HOBBY_WEIGHT = 2;
+
 
     /** Calcule le poids de l’arête entre host et visitor dans le graphe modèle.
      * Doit faire appel à la méthode compatibleWithGuest(Teenager) de Teenager.
@@ -19,32 +21,31 @@ public class AffectationUtil {
      * @return (double) - The edge weight
      */
     public static double weight(Teenager host, Teenager visitor) {
-        double weigh = 100;
-        if(!host.compatibleWithGuestGraphe(visitor)) weigh += 100;
+        double weight = 100.0;
+        if(!host.compatibleWithGuestGraphe(visitor)) weight += 100;
 
         try {
-            weigh += hobbies_weight(host, visitor);
+            weight -= hobbiesWeight(host, visitor);
         } catch(RequirementNotFound e) {
-            System.out.println("Une des deux personnes n'a pas de hobbies définis !");
+            System.out.println("Une des deux personnes n'a pas de hobby défini !");
         }
 
-        return weigh;
+        return weight;
     }
 
     /**
      * Calculate the weight value withe the hobbies to remove from the total weight
      * @param host (Teenager) - The teenager
      * @param visitor (Teenager) - The other teenager
-     * @return (double) - The edge weight got from the hobbies
+     * @return (int) - The edge weight got from the hobbies
      */
-    public static double hobbies_weight(Teenager host, Teenager visitor) throws RequirementNotFound {
-        double hobbies_weight = 0;
-        int hobbies_count = 0;
+    public static int hobbiesWeight(Teenager host, Teenager visitor) throws RequirementNotFound {
+        int hobbiesCount = 0;
         for(String hobby : host.getHobbies()){
-            if(visitor.getHobbies().contains(hobby) && hobbies_count < MAXHOBBIESCOUNT) {
-                hobbies_count++;
+            if(visitor.getHobbies().contains(hobby) && hobbiesCount <= MAX_HOBBIES_COUNT) {
+                hobbiesCount++;
             }
         }
-        return hobbies_weight;
+        return HOBBY_WEIGHT*hobbiesCount;
     }
 }
