564d16717d0be284995b5fce58faedded5e783dc
DocLyxing <leroy.el@outlook.com>
2023-05-21 03:38:39 +0200

    [Modified] Wrote InvalidCriterion

--- src/ullile/sae201/exception/InvalidCriterion.java
+++ src/ullile/sae201/exception/InvalidCriterion.java
@@ -1,3 +1,5 @@
 package ullile.sae201.exception;
 
-public class InvalidCriterion extends Exception{ }
+public class InvalidCriterion extends Exception{ 
+    public InvalidCriterion(){super("Invalid criterion");};
+}
