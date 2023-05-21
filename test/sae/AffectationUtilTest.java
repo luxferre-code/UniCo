e8a3e602be45fe056d25e2be7805b3f7541fbb47
DocLyxing <leroy.el@outlook.com>
2023-05-21 03:36:51 +0200

    [Modified] Continued class test but issue with allergies

--- test/sae/AffectationUtilTest.java
+++ test/sae/AffectationUtilTest.java
@@ -1,7 +1,17 @@
 package sae;
 
-import java.beans.Transient;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.Calendar;
+import java.util.Date;
+
+import ullile.sae201.CriterionName;
+import ullile.sae201.Teenager;
+import ullile.sae201.exception.InvalidCriterion;
+import ullile.sae201.graphe.AffectationUtil;
 
 /**
  * @author Elise Leroy
@@ -12,7 +22,7 @@ public class AffectationUtilTest {
     private Date d1, d2, d3, d4;
     
     @Before
-    public void setUp(){
+    public void setUp() throws InvalidCriterion{
     /*Instancing birth dates */
         d1 = new Date();
         d2 = new Date();
@@ -38,31 +48,30 @@ public class AffectationUtilTest {
         //t1
         t1.addRequirement(CriterionName.GUEST_ANIMAL_ALLERGY, "yes");
         t1.addRequirement(CriterionName.HOST_HAS_ANIMAL, "no");
-        t1.addRequirement(CriterionName.GENDER, "male");
-        t1.addRequirement(CriterionName.PAIR_GENDER, "female");
-        t1.addRequirement(CriterionName.HOBBIES, "video games,music");
+        t1.addRequirement(CriterionName.HOBBIES, "video games,music,sport,film");
         //t2
         t2.addRequirement(CriterionName.GUEST_ANIMAL_ALLERGY, "no");
         t2.addRequirement(CriterionName.HOST_HAS_ANIMAL, "yes");
-        t2.addRequirement(CriterionName.GENDER, "female");
-        t2.addRequirement(CriterionName.GUEST_FOOD, "vegetarian");
         t2.addRequirement(CriterionName.HOBBIES, "music");
-        t2.addRequirement(CriterionName.HISTORY, "other");
         //t3
         t3.addRequirement(CriterionName.GUEST_ANIMAL_ALLERGY, "no");
         t3.addRequirement(CriterionName.HOST_HAS_ANIMAL, "no");
-        t3.addRequirement(CriterionName.GENDER, "other");
-        t3.addRequirement(CriterionName.PAIR_GENDER, "male");
-        t3.addRequirement(CriterionName.HOBBIES, "video games");
+        t3.addRequirement(CriterionName.HOBBIES, "video games,music,flowers,film, party");
         //t4
-        t4.addRequirement(CriterionName.GUEST_ANIMAL_ALLERGY, "yes");
+        t4.addRequirement(CriterionName.GUEST_ANIMAL_ALLERGY, "no");
         t4.addRequirement(CriterionName.HOST_HAS_ANIMAL, "yes");
-        t4.addRequirement(CriterionName.GENDER, "male");
+        t4.addRequirement(CriterionName.HOBBIES, "flowers, party");
     }
 
     @Test
     public void testWeight(){
-
+        //Double.valueOf nécessaire car ambiguité voir : https://stackoverflow.com/questions/1811103/java-junit-the-method-x-is-ambiguous-for-type-y
+        //assertEquals(Double.valueOf(199), AffectationUtil.weight(t1, t2));        
+        assertEquals(Double.valueOf(97), AffectationUtil.weight(t1, t3)); //passed
+        //assertEquals(Double.valueOf(200), AffectationUtil.weight(t1, t4)); //passed
+        assertEquals(Double.valueOf(99), AffectationUtil.weight(t2, t3)); //passed
+        assertEquals(Double.valueOf(100), AffectationUtil.weight(t2, t4)); //passed
+        assertEquals(Double.valueOf(98), AffectationUtil.weight(t3, t4)); //passed
     }
 
 }
