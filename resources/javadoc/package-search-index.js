edaf3f137ee952bbef21748d1399837add48b374
DEGEZ Romain <romain.degez.etu@univ-lille.fr>
2023-06-16 22:47:55 +0200

    gen javadoc complete sae201 graphe exception

--- resources/javadoc/package-search-index.js
+++ resources/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ullile.sae201"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ullile.sae201"},{"l":"ullile.sae201.exception"},{"l":"ullile.sae201.graphe"}]
